349224ab31af1393e277ef807e1a50621fa92a39
Daiyaan <daiyaansallie7@gmail.com>
2026-02-16 15:41:11 +0200

    add: mcs0n3 and mcsOn2A

---
+++ 12practical/MaximumContiguousSubvector.java
@@ -0,0 +1,93 @@
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class MaximumContiguousSubvector {
+    public static void main(String[] args) {
+        int n = 1   0^2; 
+	    int[] X = new int[n];
+        Random rand = new Random();
+
+        int countP = 0;
+	    int countM = 0;
+        
+        // Generate array of random integers
+        for (int i = 0; i < n; i++) {
+            int randomValue = rand.nextInt(n) + 1;
+            int sign = rand.nextBoolean() ? -1: 1;
+
+            X[i] = randomValue * sign;
+
+            // Count positives and negatives (minuses)
+            if (X[i] < 0) {
+                countM++;
+            } else {
+                countP++;
+            }
+
+            // [randint(1,n)*(-1)**randint(2,4) for k in range(n)]
+        }
+
+        System.out.printf("countM = %d  countP = %d%n", countM, countP);
+        System.out.println(Arrays.toString(X));
+        
+        System.out.println(mcsOn3(X));
+        System.out.println(mcsOn2A(X));
+        System.out.println(mcsOn2B(X));
+        System.out.println(mcsOn(X));
+        
+        System.out.println("-".repeat(20));
+    }
+
+    /**
+     * O(n^3) implementation
+     */
+    private static int mcsOn3(int[] X) {
+        int n = X.length;
+        int maxsofar = 0;
+        int sum;
+        int count = 0;  // to count number of repetitions
+
+        for (int low = 0; low < n; low++) {  // [0, n)
+            for (int high = low; high < n; high++) {  // [low, n)
+                sum = 0;
+                for (int r = low; r < high; r++) {  // [low, high)
+                    count++;
+
+                    sum += X[r];
+                    if (sum > maxsofar) {
+                        maxsofar = sum;
+                    }
+                }
+            }
+        }
+
+        System.out.println("mcsOn3 Count: " + count);
+        return maxsofar;
+    }
+ 
+    /**
+     * O(n^2) implementation
+     */
+    private static int mcsOn2A(int[] X) {
+        int n = X.length;
+        int maxsofar = 0;
+        int count = 0;
+
+        for (int low = 0; low < n; low++) {  // [0, n)
+            int sum = 0;
+
+            for (int r = low; r < n; r++) {  // [low, n)
+                count++;
+
+                sum += X[r];
+                if (sum > maxsofar) {
+                    maxsofar = sum;
+                }
+            }
+        }
+
+        System.out.println("mcsOn2A Count: " + count);
+        return maxsofar;
+    }
+}
