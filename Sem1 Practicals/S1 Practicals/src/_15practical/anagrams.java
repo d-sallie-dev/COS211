3eff20c826de550e70a54d066c1345dbc24ba3e4
Daiyaan <daiyaansallie7@gmail.com>
2026-03-09 15:09:00 +0200

    add method for creating anagram signatures

--- Sem1 Practicals/S1 Practicals/src/_15practical/anagrams.java
+++ Sem1 Practicals/S1 Practicals/src/_15practical/anagrams.java
@@ -1,8 +1,20 @@
 
 
+import java.util.Arrays;
+
 public class anagrams {
-    
+    static String signature (String word) {
+        char[] charArray = word.toCharArray();
+
+        Arrays.sort(charArray);
+        String sig = new String(charArray); // the signature
+        System.out.println("sig: " + sig);
+
+        return sig;
+    }
+
     public static void main(String[] args) {
+        signature("dear");
 
         if (args.length < 1) {
             System.out.println("Usage: ./anagrams inputfile.\n\tYou gave no arguments");
