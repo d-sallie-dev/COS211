1beab25d58b4ea8e27eb4a032d4a25c9d5d0c95c
Daiyaan <daiyaansallie7@gmail.com>
2026-03-09 15:07:48 +0200

    validate program usage

--- Sem1 Practicals/S1 Practicals/src/_15practical/anagrams.java
+++ Sem1 Practicals/S1 Practicals/src/_15practical/anagrams.java
@@ -1,8 +1,16 @@
 
 
 public class anagrams {
-
+    
     public static void main(String[] args) {
+
+        if (args.length < 1) {
+            System.out.println("Usage: ./anagrams inputfile.\n\tYou gave no arguments");
+            return;
+        }
+        
+        String inputFileName = args[0]; // should be a textfile name
+
         System.out.println();
     }
 }
